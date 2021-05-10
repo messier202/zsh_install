bbd0fde0cfe7d871cce66b7ed057d237bb35e005
Messier <messier101010@gmail.com>
2021-05-10 11:31:30 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ apt install -y zsh
 mkdir tmp
 tar xf pkgs.tar.xz -C tmp
 cd tmp
-cp -r .oh-my-zsh ~
-cp -r .zshrc ~
+cp -r .oh-my-zsh $HOME
+cp -r .zshrc $HOME
 sh fonts/install.sh
 cp *.otf $HOME/.local/share/fonts
 cd ..
